b8a40b78a3289c23dc44a9f74141a90a04c89038
hiroshi <hiroshi@tanuki.page>
2024-09-15 01:53:14 -0400

    Move to using Android.bp
    legacy Android.mk declarations are problematic, especially when copying ELF files

--- config.mk
+++ config.mk
@@ -1,10 +1,11 @@
 BUILD_PATH := packages/apps/ViPER4AndroidFX
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(BUILD_PATH)/vendor,vendor)
+PRODUCT_SOONG_NAMESPACES += $(BUILD_PATH)
+
 
 PRODUCT_PACKAGES += \
-    ViPER4AndroidFX
+    ViPER4AndroidFX \
+    libv4a_re
 
 # Time to relax
 RELAX_USES_LIBRARY_CHECK := true
